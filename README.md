a979803cf4cb6458709ec00b7d6d903d05eb4bf1
Adam Ibrahimi <ibradam1@umbc.edu>
2023-02-03 01:09:58 -0500

    Pseudocode in progress

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rock_paper_scissors
 Odin Project Rock Paper Scissors in Javascript
+
+Problem: For a rock paper scissors game, we need the computer to select a
+random choice of rock, paper or scissors. Then we need to get the player's choice.
+Depending on the player's choice compared to the computer's, the player will 
+either win, lose, or tie. This will make up one round. Finally, the 
+program must play 5 rounds of this game that keeps score andd reports
+a winner or loser at the end. 
+
+
---
+++ javascript.js
@@ -0,0 +1,21 @@
+//Generate random choice for computer
+    //Get a random number from 0 to 2
+    //Assign each number to correspond to choice: 0=Rock, 1=Paper, 2=Scissors
+    //Return computer's choice as a string
+
+
+
+
+
+//Get player's choice
+    //prompt for the player's choice
+    //convert the player's choice to lowercase, then capitalize the first letter.
+    //if the capitalized player's choice is not Rock, Paper, or Scissors, then promt again (repeat the loop)
+    //return the player's choice
+
+
+
+
+
+//Determine who wins the round
+    //if player and computer pick the same, then its a tie.
